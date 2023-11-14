eb315644e543f58c4eabdf72bb1e94d4da924d8f
Hlay Aliotte <ashoobe@hotmail.com>
2023-11-14 20:40:48 +0000

    Working on Fontend

--- allbooksProject/Books/views.py
+++ allbooksProject/Books/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "Books/index.html")
+
+def login(request):
+    return render(request, "Books/login.html")
