712b15d66d44e53f282d101ca11d7668cea236bf
Eyedoljin <eyedoljin@yahoo.fr>
2023-11-15 07:38:13 +0000

    Signup html content

--- allbooksProject/Books/urls.py
+++ allbooksProject/Books/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     #path("login/", views.login,name="login"),
-    path('login/', auth_views.LoginView.as_view(template_name='Books/login.html'), name='login')
+    path('login/', auth_views.LoginView.as_view(template_name='Books/login.html'), name='login'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 ]
