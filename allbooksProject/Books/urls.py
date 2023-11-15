7d6a17c6750fae9086596d1d9e28d2c1cbd41e2c
Eyedoljin <eyedoljin@yahoo.fr>
2023-11-15 02:05:30 +0000

    Add login backend

--- allbooksProject/Books/urls.py
+++ allbooksProject/Books/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login,name="login"),
+    #path("login/", views.login,name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='Books/login.html'), name='login')
 ]
