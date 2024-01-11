a00d79517933e7e56beac7c3668d322b1b4ab8fc
Eyedoljin <eyedoljin@yahoo.fr>
2024-01-11 20:00:12 +0000

    Updated settings with email Backend

--- allbooksProject/allbooks/urls.py
+++ allbooksProject/allbooks/urls.py
@@ -24,4 +24,8 @@ urlpatterns = [
     path("",views.home,name="home"),
     path("blog",views.blog,name="blog"),
     path("contact",views.contact,name="contact"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
